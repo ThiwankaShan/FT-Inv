eb0ee87839c334e1201ec8134661137d8e0ce09d
Dilshan1997 <dmpblackedition@gmail.com>
2020-07-17 13:40:21 +0530

    fix

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,6 +8,13 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
+    // Create a new controller instance
+
+     // @return void
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
 
 
     /**
