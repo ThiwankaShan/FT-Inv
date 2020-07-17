cffbcfe654cf2f818fb25fc917762650473292f5
MEKSankalpa <66584748+MEKSankalpa@users.noreply.github.com>
2020-07-17 14:18:43 +0530

    Ft 012 create db (#14)
    * Readme updated
    
    * created db
    
    Co-authored-by: unknown <shane.thiwanka@gmail.com>
    Co-authored-by: [Keshan Sankalpa] <[m.e.k.sankalpa@gmail.com]>

---
+++ app/Division.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Division extends Model
+
+{
+   //creating the relation between Division And SubDivision
+   public function subdivision(){
+    return   $this->hasMany('App\SubDivision');
+   }
+   
+   //creating the relation between Division And Category
+   public function category(){
+       return $this->hasMany('App\Category');
+   }
+
+   //creating the relation between Division And subcategory
+   public function subcategory(){
+    return $this->hasMany('App\SubCategory');
+   }
+}
---
+++ app/SubCategory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SubCategory extends Model
+{
+    //creating the relation between Division And SubCategory
+   public function division(){
+    return $this->belongsTo('App\Division');
+  }
+
+    //creating the relation between subdivision And subcategory
+    public function subdivision(){
+    return $this->belongsTo('App\SubDivision');
+    }
+
+    //creating the relation between Category And subcategory
+    public function category(){
+        return $this->belongsTo('App\Category');
+    }
+}
