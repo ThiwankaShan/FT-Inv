43bd59e5749c4d78bcba85f32bece913d6d80603
unknown <shane.thiwanka@gmail.com>
2020-07-23 16:47:59 +0530

    live search added to navigation bar

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,4 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 Route::resource('/user', 'UserController');
 Route::resource('/item', 'ItemController');
+Route::post('/home', 'LiveSearchController@autofill')->name('liveSearch');
