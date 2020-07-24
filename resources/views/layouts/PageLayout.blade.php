4c7f9197cba7f3e215f7328c68f45c724bb47686
[Keshan Sankalpa] <[m.e.k.sankalpa@gmail.com]>
2020-07-24 08:29:48 +0530

    removed yahraj pack

--- resources/views/layouts/PageLayout.blade.php
+++ resources/views/layouts/PageLayout.blade.php
@@ -13,7 +13,7 @@
     <script src="{{ asset('js/app.js') }}" defer></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
-
+    
      
 
     <!-- Fonts -->
