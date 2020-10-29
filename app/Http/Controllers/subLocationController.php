c210f52c10a5afe19ea5c72f511f1098fe5198e8
Dilshan1997 <dmpblackedition@gmail.com>
2020-10-29 09:48:16 +0530

    correction

--- app/Http/Controllers/subLocationController.php
+++ app/Http/Controllers/subLocationController.php
@@ -1,50 +1,51 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\subLocation;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
 class SubLocationController extends Controller
 {
-	
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
 
-    public function insert(){
+	public function __construct()
+	{
+		$this->middleware('auth');
+	}
 
-        return view('pages.user');
-    }
-    //validation
-    public function storeSubLocation(Request $request){
-        $rules = [
+	public function insert()
+	{
+
+		return view('pages.user');
+	}
+	//validation
+	public function storeSubLocation(Request $request)
+	{
+		$rules = [
 			// 'location_code' => 'required|string|unique:locations',
 			// 'location_name' => 'required|string',
 
 		];
-		$validator = Validator::make($request->all(),$rules);
+		$validator = Validator::make($request->all(), $rules);
 		if ($validator->fails()) {
 			return redirect('insert')
-			->withInput()
-			->withErrors($validator);
-		}
-		else{
-            $data = $request->input();
+				->withInput()
+				->withErrors($validator);
+		} else {
+			$data = $request->input();
 
-			try{
-                $sublocation = new subLocation;
-                $sublocation->Location_code = $data['Location_code'];
-                $sublocation->subLocation_code = $data['sub_location_code'];
-                $sublocation->subLocation_name = $data['sub_location_name'];
+			try {
+				$sublocation = new subLocation;
+				$sublocation->Location_code = $data['Location_code'];
+				$sublocation->subLocation_code = $data['sub_location_code'];
+				$sublocation->subLocation_name = $data['sub_location_name'];
 
 				$location->save();
-				return redirect('insert')->with('status',"Insert successfully");
-			}
-			catch(Exception $e){
-				return redirect('insert')->with('failed',"operation failed");
+				return redirect('insert')->with('status', "Insert successfully");
+			} catch (Exception $e) {
+				return redirect('insert')->with('failed', "operation failed");
 			}
 		}
-    }
+	}
 }
