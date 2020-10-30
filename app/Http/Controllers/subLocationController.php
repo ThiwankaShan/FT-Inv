ea21e6001c7acd94cae3b4059e516b8820b33c28
Dilshan1997 <dmpblackedition@gmail.com>
2020-10-30 19:27:21 +0530

    solve problem

---
+++ app/Http/Controllers/subLocationController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\SubLocation;
+use App\Location;
+
+class subLocationController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $Locations = Location::all();
+
+        return view('forms.createSubLocation', compact('Locations'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+
+        $validatedData = $request->validate([
+            'subLocation_name' => 'required|unique:sub_locations',
+            'subLocation_code' => 'required|unique:sub_locations',
+        ]);
+
+        $subLocation = new SubLocation;
+        $subLocation->Location_code = $request->Location_code;
+        $subLocation->subLocation_name = $request->subLocation_name;
+        $subLocation->subLocation_code = $request->subLocation_code;
+        $subLocation->save();
+        return redirect('/sublocation')->with('success', "Created successfully");
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
