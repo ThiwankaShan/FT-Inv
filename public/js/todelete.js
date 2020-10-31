fdbc74de445696933919ae350418831f32b735ce
Dilshan1997 <dmpblackedition@gmail.com>
2020-10-31 10:21:38 +0530

    Delete confirmation

---
+++ public/js/todelete.js
@@ -0,0 +1,29 @@
+$('.delete-confirm').on('click', function (event) {
+    // event.preventDefault();
+    var current_object = $(this);
+    swal({
+        title: 'Are you sure?',
+        text: 'This record and it`s details will be permanantly deleted!',
+        icon: 'warning',
+        buttons: {
+            cancel: true,
+            confirm: true,
+          },
+        dangerMode: true,
+        cancelButtonClass: '#DD6B55',
+        confirmButtonColor: '#dc3545',
+        confirmButtonText: 'Delete!',
+    }).then(function(result) {
+        if (result) {
+        var action = current_object.attr('data-action');
+        var token = jQuery('meta[name="csrf-token"]').attr('content');
+        var id = current_object.attr('data-id');
+
+                    $('body').html("<form class='form-inline remove-form' method='post' action='"+action+"'></form>");
+                    $('body').find('.remove-form').append('<input name="_method" type="hidden" value="delete">');
+                    $('body').find('.remove-form').append('<input name="_token" type="hidden" value="'+token+'">');
+                    $('body').find('.remove-form').append('<input name="id" type="hidden" value="'+id+'">');
+                    $('body').find('.remove-form').submit();
+        }
+    });
+});
