48ec36fa23e3fb64ce0e55b981a3594f1a4d82bf
Shan <shane.thiwanka@gmail.com>
2020-12-06 17:56:53 +0530

    added comments

--- app/Http/Controllers/reportsController.php
+++ app/Http/Controllers/reportsController.php
@@ -12,14 +12,18 @@ class reportsController extends Controller
         $this->middleware('auth');
     }
 
-    public function pdfReport(){
+    public function pdfReport()
+    {   // genrate a pdf from filterd item details
 
+        //getting items from filter session
         $items = session('items_download');
         $grandTotal = 0;
         foreach ($items as $item){
             $grandTotal = $grandTotal + $item['rate'];
 
         }
+
+        //This is for testing purposes
         //return view('reports.template',compact('items','grandTotal'));
 
         $pdf = PDF::loadView('reports.template',compact('items','grandTotal'));
