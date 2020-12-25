87925f6acc6d4e6caa9757ff05d73386971ce8b0
Shan <shane.thiwanka@gmail.com>
2020-12-25 09:31:35 +0530

    completed supplier crud

--- app/Http/Controllers/SupplierController.php
+++ app/Http/Controllers/SupplierController.php
@@ -5,16 +5,54 @@ namespace App\Http\Controllers;
 use Validator;
 use App\Supplier;
 use Illuminate\Http\Request;
-
+use Log;
 class SupplierController extends Controller
 {
     public function __construct()
     {
         $this->middleware('auth');
     }
+
+    public function index(){
+        $suppliers = Supplier::all();
+        return view('pages.supplier',compact('suppliers'));
+    }
+
+    public function edit($supplier)
+    {
+        $supplier = Supplier::find($supplier);
+        return view('forms.supplier_forms.editSupplier',compact('supplier'));
+    }
+
+
+    public function update(Request $request)
+    {
+        $validatedata = $request->validate([
+            'supplier_name' => 'required',
+            'supplier_address' => 'nullable',
+            'telephone_number' => 'nullable|numeric',
+            'email' => 'nullable|email',
+            'vat_register_no' => 'required'
+        ]);
+
+        $supplier = Supplier::find($request->supplier_code);
+        $supplier->update($validatedata);
+
+        return view('forms.supplier_forms.editSupplier',compact('supplier'))->with('status','supplier details updated sucessfully');
+    }
+
+    public function delete($supplier)
+    {
+        $supplier = Supplier::find($supplier);
+        $supplier->delete();
+        
+        $suppliers = Supplier::all();
+        return view('pages.supplier',compact('suppliers'));
+    }
+
     public function create()
     {
-        return view('forms.supplier_forms.supplier');
+        return view('forms.supplier_forms.createSupplier');
     }
 
     public function store(Request $request)
---
+++ resources/views/tables/supplier_table.blade.php
@@ -0,0 +1,44 @@
+<div class="container" id="supplier_container">
+  <div class="row justify-content-md-center">
+    <a class="btn btn-primary text-light" href="{{ route('supplier.create') }}">Create</a>
+  </div>
+
+  <div class="row mt-4">
+    <table class="table" id="supplierTable">
+      <thead>
+        <tr>
+        <th scope="col">Supplier Code</th>
+          <th scope="col">Supplier Name</th>
+          <th scope="col">Supplier Address</th>
+          <th scope="col">Telephone Number</th>
+          <th scope="col">Email</th>
+          <th scope="col">Vat Registration Number</th>
+          <th scope="col">Action</th>
+        </tr>
+      </thead>
+      <tbody id="location_data">
+        @foreach($suppliers as $supplier)
+        <tr>
+          <td scope="row">{{$supplier->supplier_code}}</td>
+          <td>{{$supplier->supplier_name}}</td>
+          <td>{{$supplier->supplier_address}}</td>
+          <td>{{$supplier->telephone_number}}</td>
+          <td>{{$supplier->email}}</td>
+          <td>{{$supplier->vat_register_no}}</td>
+          <td class="">
+            @if(auth()->user()->role == 'admin')
+
+            <a class="btn btn-primary text-light highlight_row" href="/supplier/edit/{{$supplier->supplier_code}}">Edit</a>
+            <a class="btn btn-danger delete-item text-light"  href="/supplier/delete/{{$supplier->supplier_code}}" data-method="POST" token='{!! csrf_token() !!}'>Delete</a>
+           
+
+            @elseif(auth()->user()->role == 'manager')
+            <a class="btn btn-primary text-light" href="/supplier/edit/{{$supplier->supplier_code}}">Edit</a>
+            @endif
+          </td>
+        </tr>
+        @endforeach
+      </tbody>
+    </table>
+  </div>
+</div>
