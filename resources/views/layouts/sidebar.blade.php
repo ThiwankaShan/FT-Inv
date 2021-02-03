2931c4c99598326b33709ffe20f6c00ad2337033
Dilshan Madhuranga <48722230+Dilshan1997@users.noreply.github.com>
2021-02-03 11:56:28 +0530

    correct the sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,44 +1,45 @@
-<div class="sidebar">
-    <ul class="list-unstyled components">
+<div class="sidebar" >
+ <div id="side_bar">
+    <ul class="list-unstyled components" >
         <li>
-            <a href="/"><i class="fa fa-lg fa-home"></i><span class="collapse" id="side_bar">Home</span></a>
+            <a href="/"><i class="fa fa-lg fa-home"></i><span  >Home</span></a>
         </li>
         <li>
-            <a href="/home"><i class="fa fa-lg fa-tachometer"></i><span class="collapse" id="side_bar">Dashboard</span></a>
+            <a href="/home"><i class="fa fa-lg fa-tachometer"></i><span  >Dashboard</span></a>
         </li>
         <li>
-            <a href="{{route('item.create')}}"><i class="fa fa-lg fa-sitemap"></i><span class="collapse" id="side_bar">Create
+            <a href="{{route('item.create')}}"><i class="fa fa-lg fa-sitemap"></i><span  >Create
                     Items</span></a>
 
         </li>
         <li>
-            <a href="{{ route('reports.create') }}"><i class="fa fa-lg fa-suitcase"></i><span class="collapse" id="side_bar">Reports</span></a>
+            <a href="{{ route('reports.create') }}"><i class="fa fa-lg fa-suitcase"></i><span  >Reports</span></a>
 
         </li>
-
+        </div>
 
         <li>
-            <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
-                <i class="fa fa-lg fa-suitcase"></i><span class="collapse" id="side_bar">Inventory</span>
+            <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="true" class="dropdown-toggle" id="side_bar">
+                <i class="fa fa-lg fa-suitcase"></i><span >Inventory</span>
             </a>
-            <ul class="collapse list-unstyled custom-menue" id="pageSubmenu">
+            <ul class="collapse list-unstyled custom-menue " id="pageSubmenu" class="collapse">
                 <li>
-                    <a href="{{ route('supplier.index') }}"><i class="fa fa-lg fa-address-card"></i><span class="collapse" id="side_bar">Suppliers</span></a>
+                    <a href="{{ route('supplier.index') }}"><i class="fa fa-lg fa-address-card"></i><span  >Suppliers</span></a>
                 </li>
                 <li>
-                    <a href="{{ route('location.index') }}"><i class="fa fa-lg fa-map-marker"></i><span class="collapse" id="side_bar">Locations</span></a>
+                    <a href="{{ route('location.index') }}"><i class="fa fa-lg fa-map-marker"></i><span  >Locations</span></a>
                 </li>
                 <li>
-                    <a href="{{ route('subLocation.index') }}"><i class="fa fa-lg fa-suitcase"></i><span class="collapse" id="side_bar">Sub Locations</span></a>
+                    <a href="{{ route('subLocation.index') }}"><i class="fa fa-lg fa-suitcase"></i><span  >Sub Locations</span></a>
                 </li>
                 <li>
-                    <a href="{{ route('category.index') }}"><i class="fa fa-lg fa-suitcase"></i><span class="collapse" id="side_bar">Category</span></a>
+                    <a href="{{ route('category.index') }}"><i class="fa fa-lg fa-suitcase"></i><span  >Category</span></a>
                 </li>
                 <li>
-                    <a href="{{ route('subcategory.index') }}"><i class="fa fa-lg fa-suitcase"></i><span class="collapse" id="side_bar">Sub Category</span></a>
+                    <a href="{{ route('subcategory.index') }}"><i class="fa fa-lg fa-suitcase"></i><span  >Sub Category</span></a>
                 </li>
                 <li>
-                    <a href="{{ route('grn.show',1) }}"><i class="fa fa-lg fa-suitcase"></i><span class="collapse" id="side_bar">GRN</span></a>
+                    <a href="{{ route('grn.show',1) }}"><i class="fa fa-lg fa-suitcase"></i><span  >GRN</span></a>
                 </li>
             </ul>
         </li>
@@ -46,3 +47,7 @@
 
     </ul>
 </div>
+<script>
+
+
+</script>
