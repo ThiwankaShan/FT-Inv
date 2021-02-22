3d3a7ede8f637071fee93fda850126b4f2cadbf5
Dilshan Madhuranga <48722230+Dilshan1997@users.noreply.github.com>
2021-02-22 09:55:19 +0530

    modifying side bar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,6 +1,6 @@
 <div class="sidebar" >
- <div id="side_bar">
-    <ul class="list-unstyled components" >
+ <div >
+    <ul class="list-unstyled components" id="side_bar">
         <li>
             <a href="/"><i class="fa fa-lg fa-home"></i><span  >Home</span></a>
         </li>
@@ -21,12 +21,15 @@
             <a href="{{ route('dispose.index') }}"><i class="fa fa-lg fa-suitcase"></i><span >Bin</span></a>
         </li>
 
-       
+
 
         <li>
-            <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="true" class="dropdown-toggle" id="side_bar">
+            <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle" id="side_bar">
                 <i class="fa fa-lg fa-suitcase"></i><span >Inventory</span>
             </a>
+
+            </li>
+    </ul>
             <ul class="collapse list-unstyled custom-menue " id="pageSubmenu" class="collapse">
                 <li>
                     <a href="{{ route('supplier.index') }}"><i class="fa fa-lg fa-address-card"></i><span  >Suppliers</span></a>
@@ -46,13 +49,12 @@
                 <li>
                     <a href="{{ route('grn.show',1) }}"><i class="fa fa-lg fa-suitcase"></i><span  >GRN</span></a>
                 </li>
-                
+
             </ul>
-        </li>
 
 
 
-    </ul>
+
 
 </div>
 </div>
@@ -60,3 +62,4 @@
 
 
 </script>
+
